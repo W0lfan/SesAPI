7db35848d7c42ba8b30dd95674fc27d59e753fa8
naflouille <naflouille@proton.me>
2023-12-16 21:25:01 +0100

    Add files via upload

--- editor/edit/src/app.jsx
+++ editor/edit/src/app.jsx
@@ -1,28 +1,14 @@
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
 import appSvg from '../../../public/modules/utilities/svg';
 import appInput from '../../../public/modules/utilities/input';
+import ArticleBody from './components/body';
+import './index.css';
+import '../../../public/styling/root/index.css'
+import Toolsbar from './components/toolbar';
+import Fields from './components/fields';
 
 
 
-const Toolsbar = ( { article } ) => {
-  return (
-    <div className="toolsbar">
-      <a href='../../../'>
-        {appSvg.new('logo')}
-      </a>
-      <div className="tools-container">
-          <div className="article-informations">
-              {appInput.new('input','Article name', article.general.name || 'New article','articleName',['textInput'])}
-              {appSvg.new('edit')}
-          </div>
-      </div>
-    </div>
-  )
-};
-Toolsbar.propTypes = {
-  article : PropTypes.object.isRequired,
-};
 
 function App() {
   let article;
@@ -39,6 +25,7 @@ function App() {
   return (
     <>
       <Toolsbar article={article}/>
+      <ArticleBody article = {article} />
       <div className="popup-container-dnd"></div>
     </>
   );
---
+++ editor/edit/src/components/resources/other/image/view.jsx
@@ -0,0 +1,80 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import save from '../../../../modules/save';
+import appInput from '../../../../../../../public/modules/utilities/input';
+import { Tools } from '../../../fields/field';
+import ActualizePopUp, { popup } from '../../../../../../../public/modules/utilities/popup';
+import { DefaultArticle } from '../../../body';
+
+
+const ImageRender = ( {image, imageIndex, fieldIndex} ) => {
+    const [ img, setImg ] = useState(image.src);
+
+
+    return (
+        <div className={"image-resource" + " " + (imageIndex%2 == 0 ? "right" : "left")} key={imageIndex} id={image.id}>
+            <>
+                <div className="image">
+                    <img src={img} />
+                </div>
+                <div className="content">
+                    <div className="input-container">
+                        <div className="box-info">Image link</div>
+                        {appInput.new('input','Image link',image.src,'ImageLink',[],function(event) {
+                            let value = event.target.value;
+                            DefaultArticle.content[fieldIndex].visualResources[imageIndex].src = value;
+                            save(DefaultArticle);
+                            setImg(value)
+                        })}
+                    </div>
+                    <div className="input-container">
+                        <div className="box-info">Image label</div>
+                        {appInput.new('input','Image label',image.label,'ImageLabel',[],function(event) {
+                            let value = event.target.value;
+                            DefaultArticle.content[fieldIndex].visualResources[imageIndex].label = value;
+                            save(DefaultArticle);
+                        })}
+                    </div>
+                    <div className="input-container">
+                        <div className="box-info">Image description</div>
+                        {appInput.new('textarea','Image label',image.label,'ImageDescription', [], function(event) {
+                            let value = event.target.value;
+                            DefaultArticle.content[fieldIndex].visualResources[imageIndex].description = value;
+                            save(DefaultArticle);
+                        })}
+                    </div>
+                </div>
+            </>
+            <Tools funcs={{
+                delete : function() {
+                    ContentDeletion(document.getElementById(image.id),fieldIndex,imageIndex);
+                }
+            }}/>
+        </div>
+    )
+};
+
+function ContentDeletion(p,I,i) {
+    ActualizePopUp({
+        title : "Image deletion",
+        description : "Are you sure you want to delete this image?"
+    }, [{
+        type : "filled",
+        container : {text:"Delete"},
+        action: function() {
+            p.remove();
+            DefaultArticle.content[I].visualResources.splice(i,1);
+            save(DefaultArticle);
+            popup.close();
+        }
+    }])
+}
+
+ImageRender.propTypes = {
+    image : PropTypes.object.isRequired,
+    imageIndex : PropTypes.number.isRequired,
+    fieldIndex : PropTypes.number.isRequired,
+};
+
+
+export default ImageRender;
