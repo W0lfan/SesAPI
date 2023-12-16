8890853f38e8175b3c55eba248fec59fb8667297
Naflouille <69418024+W0lfan@users.noreply.github.com>
2023-12-16 14:59:41 +0100

    Updates

--- modules/utilities/input/index.jsx
+++ modules/utilities/input/index.jsx
@@ -5,12 +5,11 @@ const types = [
 ]
 
 const appInput = {
-    new  : function(type, placeholder, value, id, classNames, action = null) {
+    new  : function(type, placeholder, value, id, classNames = [], action = null) {
         if (!types.includes(type)) {
             throw new Error(`Error: type ${type} of input is unknown.`)
         } else {
             let component;
-
             if (type == 'input') {
                 component = (
                     <input 
@@ -24,8 +23,8 @@ const appInput = {
                                 })
                             ) : null
                         }
-                        onClick={
-                          action ? (() => action) : (() => { return })
+                        onInput={
+                          action
                         }
                     />
                 )
@@ -44,9 +43,26 @@ const appInput = {
                                 })
                             ) : null
                         }
-                        onClick={
-                          action ? (() => action) : (() => { return })
+                        onInput={
+                          action
                         }
+                        onKeyDown={(event) => {
+                            if (action) {
+                                if (classNames.includes('autosize')) {
+                                    const previousValue = event.target.value;
+
+                                    setTimeout(() => {
+                                        const currentValue = event.target.value;
+                                        if (event.key === "Enter" || (event.key === "Backspace" && previousValue !== currentValue)) {
+                                            event.target.style.height = 'auto'; // Reset the height to auto
+                                            event.target.style.height = (event.target.scrollHeight) + 'px'; // Set the new height
+                                        }
+                                    }, 0);
+                                }
+
+                                action(event);
+                            }
+                        }}
                     ></textarea>
                 )
             }
