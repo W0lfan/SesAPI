69e2f7b800aacfa83ffb0d53b11d1641128b2fbb
Naflouille <69418024+W0lfan@users.noreply.github.com>
2023-12-17 10:47:34 +0100

    Vite config update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SesAPI/"
+  base: "/SesAPI/",
+  resolve: {
+    alias: {
+      '@editor': path.resolve(__dirname, '/editor/edit/'),
+      // Add more aliases for other subdirectories as needed
+    },
+  },
 })
