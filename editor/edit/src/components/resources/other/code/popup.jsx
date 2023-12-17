beae8cea525f40709eb71c4ffe6a2ed591d9159a
Naflouille <69418024+W0lfan@users.noreply.github.com>
2023-12-17 10:08:18 +0100

    test

--- editor/edit/src/components/resources/other/code/popup.jsx
+++ editor/edit/src/components/resources/other/code/popup.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import appInput from "../../../../../../../public/modules/utilities/input";
 
 
@@ -16,23 +15,4 @@ const CodeDrop = () => {
     )  
 };
 
-=======
-import appInput from "../../../../../../../public/modules/utilities/input";
-
-
-const CodeDrop = () => {
-    return(
-        <div className="code-addition">
-            <div className="input-fields">
-                {appInput.new('input','Code label','','codeLabel')}
-                {appInput.new('input','Code description','','codeDescription')}
-            </div>
-            <div className="render">
-                {appInput.new('textarea','Enter code','','codeContent',["code-field"])}
-            </div>
-        </div>
-    )  
-};
-
->>>>>>> 218f891f43297a53f31bca18f62cdcc649d01f8a
 export default CodeDrop;
--- editor/edit/src/modules/save/index.jsx
+++ editor/edit/src/modules/save/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import AppTimeConverter from "../../../../../public/modules/utilities/time";
 import { DefaultArticle } from "../../components/body";
 import { storage } from "../../storage/access";
@@ -41,48 +40,4 @@ const save = (article) => {
     }
 };
 
-=======
-import AppTimeConverter from "../../../../../public/modules/utilities/time";
-import { DefaultArticle } from "../../components/body";
-import { storage } from "../../storage/access";
-
-
-
-const save = (article) => {
-    if (document.querySelector('.save-update')) {
-        const s = storage.access();
-        if (!s.articles) s.articles = [];
-        
-    
-        const indexToUpdate = s.articles.findIndex((a) => a.time.createdOn === article.time.createdOn);
-    
-        if (indexToUpdate !== -1) {
-            s.articles[indexToUpdate] = article;
-        } else {
-            s.articles.push(article);
-        }
-        
-        
-    
-        storage.set(s);
-        console.log(s)    
-        const update = () => {
-            const box = document.querySelector('.save-update');
-            const value = box.querySelector('.value');
-            const info = box.querySelector('.info');
-    
-            value.innerHTML = "Changes saved";
-            info.innerHTML = AppTimeConverter((new Date()).getTime());
-    
-            box.classList.add('active');
-            setTimeout(() => {
-                box.classList.remove('active');
-            }, 5000);
-        };
-    
-        update();
-    }
-};
-
->>>>>>> 218f891f43297a53f31bca18f62cdcc649d01f8a
 export default save;
