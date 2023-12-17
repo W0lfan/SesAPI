cc3b8cafd68a933947fc36e2260c85ea13ba12cb
Naflouille <69418024+W0lfan@users.noreply.github.com>
2023-12-17 10:03:16 +0100

    test

--- editor/edit/src/storage/access.js
+++ editor/edit/src/storage/access.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 const SesAPIStorage = 'SesAPIStorage'; // Exclusive localstorage value
 
 
@@ -18,25 +16,4 @@ if (!storage.access()) storage.set({});
 
 if (!storage.access("SesAPIParameters")) storage.set({
     preferedDisplay : "grid"
-=======
-
-const SesAPIStorage = 'SesAPIStorage'; // Exclusive localstorage value
-
-
-export const storage = {
-    access : function(type = SesAPIStorage) {
-        return JSON.parse(localStorage.getItem(type));
-    },
-    set : function(value = {}, to = SesAPIStorage) {
-        localStorage.setItem(
-            to, JSON.stringify(value)
-        )
-    }
-}
-
-if (!storage.access()) storage.set({});
-
-if (!storage.access("SesAPIParameters")) storage.set({
-    preferedDisplay : "grid"
->>>>>>> 218f891f43297a53f31bca18f62cdcc649d01f8a
 }, "SesAPIParameters" );
